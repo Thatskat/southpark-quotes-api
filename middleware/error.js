0dcbe315750652607ca1c1482e6f9acf49d05472
Thatskat <109633947+Thatskat@users.noreply.github.com>
2023-06-29 14:22:51 +1000

    winston logger

--- middleware/error.js
+++ middleware/error.js
@@ -2,5 +2,20 @@
 const winston = require("winston");
 
 const logger = winston.createLogger({
-    
-})
+  level: "info",
+  format: winston.format.json(),
+  defaultMeta: { service: "SouthPark-Quotes" },
+  transports: [
+    new winston.transports.File({
+      filename: "/logs/error.log",
+      level: "error",
+    }),
+    new winston.transports.File({
+      filename: "/logs/warnings.log",
+      level: "warn",
+    }),
+    new winston.transports.File({ filename: "/logs/combined.log" }),
+  ],
+});
+
+// EXPRESS ERRORS LOG
