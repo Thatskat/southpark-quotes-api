3dfbf68255950b8a24d0272d8ffa51284bd9f1ed
Thatskat <109633947+Thatskat@users.noreply.github.com>
2023-06-29 14:21:09 +1000

    finished getRandom function

---
+++ quotesController.js
@@ -0,0 +1,21 @@
+"use strict";
+
+let quotes = require("./quotes.js");
+
+function getRandom(quoteNumber) {
+  let quoteLimit = quoteNumber > quotes.length ? quotes.length : quoteNumber;
+
+  let response = new Array(quoteLimit);
+
+  let availableResponse = quotes.slice();
+
+  for (let i = 0; i < quoteLimit; i++) {
+    response[i] = availableResponse.splice(
+      Math.floor(Math.random() * availableResponse.length),
+      1
+    )[0];
+    return response;
+  }
+}
+
+module.exports.getRandom = getRandom;
