1c60b30f157779290e3b4268b851ae38cf2ff740
Thatskat <109633947+Thatskat@users.noreply.github.com>
2023-06-29 12:36:00 +1000

    first section of quotes added

---
+++ quotes.js
@@ -0,0 +1,159 @@
+"use strict";
+
+module.exports = [
+  {
+    quote: "I am a cop, and you will respect my authoritah!",
+    character: "Cartman",
+  },
+  {
+    quote: "Oh my god, they killed Kenny!",
+    character: "Stan",
+  },
+  {
+    quote: "You bastards!",
+    character: "Kyle",
+  },
+  {
+    quote: "Don’t you dare call me a Cartman!",
+    character: "Stan",
+  },
+  {
+    quote: "Don’t lie, Stan. Lying makes you sterile.",
+    character: "Mr. Garrison",
+  },
+  {
+    quote: "I'm not fat, I'm festively plump.",
+    character: "Cartman",
+  },
+  {
+    quote: "Drugs are bad, mmkay?",
+    character: "Mr. Mackey",
+  },
+  {
+    quote:
+      "All animals kill, and the animals that don’t kill are stupid ones like cows and turtles and stuff.",
+    character: "Kyle",
+  },
+  {
+    quote:
+      "As you get older, boobs will start becoming a major part of your life. But you can’t let them get in the way of your friends. There are a lot of boobs out there. But they’re just boobs. Your friends are forever.",
+    character: "Randy",
+  },
+  {
+    quote: "Screw you guys, I’m going home!",
+    character: "Cartman",
+  },
+  {
+    quote: "Hell, everything’s legal in Mexico. It’s the American way!",
+    character: "Jimbo",
+  },
+  {
+    quote:
+      "When a chick says ‘we need to talk,’ you might as well start punching yourself in the balls, dude.",
+    character: "Cartman",
+  },
+  {
+    quote: "Operation Human Shield my ass.",
+    character: "Chef",
+  },
+  {
+    quote:
+      "Hippies. They’re everywhere. They wanna save Earth, but all they do is smoke pot and smell bad.",
+    character: "Cartman",
+  },
+  {
+    quote:
+      "Well I’m sorry Wendy, but I just don’t trust anything that bleeds for 5 days and doesn’t die!",
+    character: "Mr. Garrison",
+  },
+  {
+    quote: "Cartman doesn’t know a rainforest from a Pop-Tart.",
+    character: "Stan",
+  },
+  {
+    quote:
+      "Kenny’s family is so poor that yesterday, they had to put their cardboard box up for a second mortgage.",
+    character: "Cartman",
+  },
+  {
+    quote:
+      "I think that parents only get so offended by television because they rely on it as a babysitter and the sole educator of their kids.",
+    character: "Kyle",
+  },
+  {
+    quote:
+      "You know, I think that if parents would spend less time worrying about what their kids watch on TV and more time worrying about what’s going on in their kids’ lives, this world would be a much better place.",
+    character: "Stan",
+  },
+  {
+    quote:
+      "We must stop frivolous spending! Instead of paying for cable, let us watch clouds! Instead of buying clothes, wear but sheets from thine beds! Cut spending to only the bare essentials! Water and bread and margaritas!",
+    character: "Randy",
+  },
+  {
+    quote:
+      "Don’t do drugs kids. There is a time and place for everything. It’s called college.",
+    character: "Chef",
+  },
+  {
+    quote: "All people from Jersey do is hump and punch each other.",
+    character: "Stan",
+  },
+  {
+    quote:
+      "Don’t you know the first law of physics? Anything that’s fun costs at least eight dollars.",
+    character: "Cartman",
+  },
+  {
+    quote:
+      "No, that’s wrong, Cartman. But don’t worry. There are no stupid answers, just stupid people.",
+    character: "Mr. Garrison",
+  },
+  {
+    quote: "Oh my god, they killed Kenny!",
+    character: "Stan",
+  },
+  {
+    quote: "Oh, I forgot to tell you that part. I'm Lorde.",
+    character: "Randy",
+  },
+  {
+    quote:
+      "Sometimes you gotta go forward by going backward. Are we driving cars or are cars driving us?",
+    character: "Matthew McConaughey",
+  },
+  {
+    quote: "I AMMM Lorde. LAWDY LAWDY LORDE!",
+    character: "Randy",
+  },
+  {
+    quote: "Winter's coming, Sharon, and I'm a busy little bee. Buzz! Buzz!",
+    character: "Randy",
+  },
+  {
+    quote: "I don't know what to believe in any more, mmkay!",
+    character: "Mr. Mackey",
+  },
+  {
+    quote:
+      "But what does being Hawaiian have to do with me being like an emo chick on her period?",
+    character: "Butters",
+  },
+  {
+    quote:
+      "Butters, people can't just go around beating up people who have diabetes!",
+    character: "Kyle",
+  },
+  {
+    quote: "I just hope when she does her pageants, the judges don't take away points because she's got a pig heart.",
+    character: "Honey Boo Boo's Mom",
+  },
+  {
+    quote: "My name's Honey Boo Boo, and I'm a beauty queen, bitches!",
+    character: "Honey Boo Boo"
+  },
+  {
+    quote: "We got crappy jewelry, now all we need are some old people.",
+    character: "Cartman"
+  }
+];
