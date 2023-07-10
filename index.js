f378f4aae5f7d4bcb0a6e83ab101c7a2f5a3ddd3
Thatskat <109633947+Thatskat@users.noreply.github.com>
2023-07-10 14:13:54 +1000

    log directory

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use(cors());
 app.use(express.json());
 app.use(limit);
 
-fs.mkdirSync(targetDir, { recursive: true });
+fs.mkdirSync(logs, { recursive: true });
 
 // USE PUBLIC FOLDER
 app.use(express.static(__dirname + "/public"));
